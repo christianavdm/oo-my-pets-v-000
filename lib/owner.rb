5b9355314d3d7ead4cdc5bbbf48c44effd607b7f
Christiana Vandermale <christianavdm@gmail.com>
2018-05-16 23:30:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,4 +51,16 @@ class Owner
     end
   end
   
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+  def feed_fish
+    @pets[:fish].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+  
 end
