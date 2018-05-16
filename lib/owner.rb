cb29a0a4764136879f43f8f1f5ede4c0eb5cc3f0
Christiana Vandermale <christianavdm@gmail.com>
2018-05-16 23:25:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,6 +31,11 @@ class Owner
     @pets[:fishes] << fish
   end
   
+    def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  
   def say_species
     return "I am a human."
   end
