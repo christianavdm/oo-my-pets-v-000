4f6ddca95fbaf59888914557275d1a5322cb293d
Christiana Vandermale <christianavdm@gmail.com>
2018-05-16 23:35:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Owner
-  attr_accessor :pets, :name
+  attr_accessor :pets, :name, :mood
   attr_reader :species, :fishes, :cats, :dogs
   attr_writer 
   
@@ -29,16 +29,19 @@ class Owner
   def buy_fish(name)
     fish = Fish.new(name)
     @pets[:fishes] << fish
+    owner.mood = "happy"
   end
   
   def buy_cat(name)
     cat = Cat.new(name)
     @pets[:cats] << cat
+    owner.mood = "happy"
   end
   
   def buy_dog(name)
     dog = Dog.new(name)
     @pets[:dogs] << dog
+    owner.mood = "the most happy!"
   end
   
   def say_species
@@ -61,6 +64,11 @@ class Owner
     @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
+  end
+  
+  def sell_pets
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+    owner.mood = "sad"
   end
   
 end
